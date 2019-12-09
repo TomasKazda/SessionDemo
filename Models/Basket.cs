317bad6dc8072ad2609229e5e9a81392b30850d1
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-12-09 13:33:41 +0100

    first commit

---
+++ Models/Basket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SessionDemo.Models
+{
+    public class Basket
+    {
+        public Guid Id { get; }
+        public decimal Price { get; set; }
+
+        [IgnoreDataMember]
+        public decimal PriceWithVat => Price * vat;
+
+        [IgnoreDataMember]
+        public string Description { get; set; }
+
+        private decimal vat { get; }
+    }
+}
---
+++ Services/DataManipulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SessionDemo.Models;
+
+namespace SessionDemo.Services
+{
+    public class DataManipulator: IEnumerable<Basket>
+    {
+        private List<Basket> _listOfBaskets;
+
+        public DataManipulator()
+        {
+            _listOfBaskets = new List<Basket>();
+        }
+
+        public void AddItem(Basket basket)
+        {
+            _listOfBaskets.Add(basket);
+        }
+
+        public IEnumerator<Basket> GetEnumerator()
+        {
+            return ((IEnumerable<Basket>)_listOfBaskets).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Basket>)_listOfBaskets).GetEnumerator();
+        }
+    }
+}
